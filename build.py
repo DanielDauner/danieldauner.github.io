d6bcfff9a86be48004ddfe2ac7fb19e77b542554
DanielDauner <daniel.dauner@gmail.com>
2024-09-14 12:35:07 +0200

    Update talks

--- build.py
+++ build.py
@@ -267,12 +267,6 @@ def get_index_html():
         </div>
         <!-- <div class="row" style="margin-top: 3em;">
             <div class="col-sm-12" style="">
-                <h4>Talks</h4>
-                {talks}
-            </div>
-        </div> 
-        <div class="row" style="margin-top: 3em;">
-            <div class="col-sm-12" style="">
                 <h4>News</h4>
                     <table>
                         <tr>
@@ -291,6 +285,12 @@ def get_index_html():
                 {pub}
             </div>
         </div>
+        <div class="row" style="margin-top: 3em;">
+            <div class="col-sm-12" style="">
+                <h4>Invited Talks</h4>
+                {talks}
+            </div>
+        </div> 
         <div class="row" style="margin-top: 3em;">
             <div class="col-sm-12" style="">
                 <h4>University Projects</h4>
